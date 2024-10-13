5d41532ffed438393691a3f93f7ee598f0851e40
rajeshmauryasde <rajeshk@sigmoidanalytics.com>
2024-10-13 18:33:49 +0530

    Refactored name

--- src/main/java/org/example/design_patterns/behavioural/strategy/Main.java
+++ src/main/java/org/example/design_patterns/behavioural/strategy/Main.java
@@ -2,9 +2,9 @@ package org.example.design_patterns.behavioural.strategy;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat1 = new Cat(new JumpAndPrey());
-        cat1.hunt();
-        Cat cat2 = new Cat(new RunAndPrey());
-        cat2.hunt();
+        Cat cat = new Cat(new JumpAndPrey());
+        cat.hunt();
+        Cat leopard = new Cat(new RunAndPrey());
+        leopard.hunt();
     }
 }
